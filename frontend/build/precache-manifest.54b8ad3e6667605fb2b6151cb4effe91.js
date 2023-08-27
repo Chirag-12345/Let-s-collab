3768e58bbc7565803857b48345d995ec908cb102
Chirag <chiragbhar45@gmail.com>
2023-08-27 17:57:55 +0530

    First Commit of project

---
+++ frontend/build/precache-manifest.54b8ad3e6667605fb2b6151cb4effe91.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "43bbf00e1318b0d64845",
+    "url": "/static/css/main.cf802607.chunk.css"
+  },
+  {
+    "revision": "43bbf00e1318b0d64845",
+    "url": "/static/js/main.43bbf00e.chunk.js"
+  },
+  {
+    "revision": "d7114c529830d38502cb",
+    "url": "/static/js/1.d7114c52.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "8ad3c8907e73a0417bad335d2440a23a",
+    "url": "/index.html"
+  }
+];
